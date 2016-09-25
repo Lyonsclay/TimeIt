53eabe0b79900e195a0619dd5a34cbd25a3269ba
Lyonsclay <lyonsclay@yahoo.com>
2016-09-25 15:58:48 -0400

    Add enzyme and shallow render Svg component.

--- __tests__/components/Clock.test.js
+++ __tests__/components/Clock.test.js
@@ -1,43 +1,34 @@
-import { Text } from 'react-native';
+/* import { Text } from 'react-native';*/
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from '../../app/reducers/rootReducer';
-import Clock from '../../app/components/clock.react';
+/* import renderer from 'react-test-renderer';*/
+import { shallow } from 'enzyme';
+/* import { Provider } from 'react-redu';*/
+/* import { createStore } from 'redux';*/
+/* import rootReducer from '../../app/reducers/rootReducer';*/
+/* import Clock from '../../app/components/clock.react';*/
+/* import Clocker from '../../app/components/clocker.react';*/
+import Svg, { Text } from 'react-native-svg';
+/* */
+/* jest.mock('Text', () => 'Text');*/
+/* describe('Test Svg Text with Jest', () => {*/
+/* const clocker = renderer.create(<Svg><Text>Hello</Text></Svg>).toJSON();*/
+/* */
+/* it('clockers', () => {*/
+/* expect(clocker).toMatchSnapshot();*/
+/* })*/
+/* })*/
+/* */
 
-describe('Clock', () => {
+const Component = (props) => {
 
-  initialState = {
-    screen: {
-      width: 0,
-      height: 0,
-      xMid: 0,
-      yMid: 0
-    },
+  return <Svg width={props.w} height={props.h}><Text>{JSON.stringify(props.happy)}</Text></Svg>;
+}
 
-    clock: {
-      diameter: 99,
-      strokeWidth: 2.5,
-      duration: 0.0,
-      moving: false
-    }
-  }
-
-  const store = createStore(
-    rootReducer,
-    initialState
-  );
-
-  const clock = renderer.create(
-    <Provider store={store}>
-    <Clock />
-    </Provider>
-  ).toJSON();
-
-  it('has a <Circle />', ()  => {
-    expect(clock).toMatchSnapshot();
-
-
-  });
-});
+describe('Test Svg Text with Enzyme', () => {
+  it('shallow renders', () => {
+    happy = { happy: 'happy', h: 120, w: 420 }
+    component = React.createElement(Component, happy);
+    const wrapper = shallow(component);
+    expect(wrapper.props().height).toBe(120);
+  })
+})
